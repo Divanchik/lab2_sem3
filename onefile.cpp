b5885f8af4983ce131718703d38826f93129caa8
Divanchik <d-sh-2016@list.ru>
2020-10-24 15:00:15 +0400

    const and other bullshit

--- onefile.cpp
+++ onefile.cpp
@@ -9,7 +9,7 @@ const char vl(186);
 const char hl(205);
 const char tv(178);
 const char fv(176);
-int count_char(char c, char *s)
+int count_char(const char c, const char *s)
 {
     int i = 0;
     for (; *s; s++)
@@ -17,7 +17,7 @@ int count_char(char c, char *s)
             i += 1;
     return i;
 }
-int int_in(int n, int a, int b)
+int int_in(const int n, const int a, const int b)
 {
     if (n >= a && n <= b)
         return 1;
@@ -30,18 +30,18 @@ class BinImage
     int n, m;
 
 public:
-    int _n(){return n;}
-    int _m(){return m;}
-    int** _f(){return f;}
-    void filling(int fill)
+    int _n() const { return n; }
+    int _m() const { return m; }
+    int **_f() const { return f; }
+    void filling(const int fill)
     {
-        if(f!=NULL)
+        if (f != NULL)
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < m; j++)
                     f[i][j] = fill;
     }
     BinImage() : f(NULL), n(0), m(0) {}
-    BinImage(int a, int b, int fill) : n(a), m(b)
+    BinImage(const int a, const int b, const int fill) : n(a), m(b)
     {
         f = (int **)malloc(n * sizeof(int));
         for (int i = 0; i < n; i++)
@@ -56,7 +56,9 @@ public:
         {
             std::cout << "Input height, then input width:" << std::endl;
             std::cin >> n >> m;
-            while (getchar() != '\n'){}
+            while (getchar() != '\n')
+            {
+            }
         }
         f = (int **)malloc(n * sizeof(int));
         for (int i = 0; i < n; i++)
@@ -67,7 +69,9 @@ public:
         for (int i = 0; i < n; i++)
         {
             fgets(buf, m + 1, stdin);
-            while (getchar() != '\n'){}
+            while (getchar() != '\n')
+            {
+            }
             for (int j = 0; j < m; j++)
             {
                 f[i][j] = buf[j] - '0';
@@ -77,17 +81,8 @@ public:
         delete buf;
         delete buf1;
     }
-    void output()
+    void output() const
     {
-        // defining special characters
-        char lt(201);
-        char lb(200);
-        char rt(187);
-        char rb(188);
-        char vl(186);
-        char hl(205);
-        char tv(178);
-        char fv(176);
         // head
         std::cout << lt;
         for (int j = 0; j < m; j++)
@@ -117,115 +112,122 @@ public:
         free(f);
     }
     // operators overload
-    int& operator()(int a, int b)
+    int &operator()(const int a, const int b) const
     {
-        if (int_in(a,0,n-1) && int_in(a,0,m-1))
+        if (int_in(a, 0, n - 1) && int_in(a, 0, m - 1))
             return f[a][b];
         else
             return f[0][0];
     }
-    BinImage& operator*(BinImage& b)
+    BinImage operator*(const BinImage &b)
     {
-        BinImage* tmp = new BinImage(n,m,0);
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                (*tmp)(i,j) = f[i][j]&b(i,j);
-        return *tmp;
+        BinImage tmp(n, m, 0);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                tmp(i, j) = f[i][j] & b(i, j);
+        return tmp;
     }
-    BinImage& operator+(BinImage& b)
+    BinImage operator+(const BinImage &b)
     {
-        BinImage* tmp = new BinImage(n,m,0);
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                (*tmp)(i,j) = f[i][j]|b(i,j);
-        return *tmp;
+        BinImage tmp(n, m, 0);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                tmp(i, j) = f[i][j] | b(i, j);
+        return tmp;
     }
-    BinImage& operator*(int l)
+    BinImage operator*(const int l)
     {
-        BinImage* tmp = new BinImage(n,m,0);
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                (*tmp)(i,j) = f[i][j]&l;
-        return *tmp;
+        BinImage tmp(n, m, 0);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                tmp(i, j) = f[i][j] & l;
+        return tmp;
     }
-    BinImage& operator+(int l)
+    BinImage operator+(const int l)
     {
-        BinImage* tmp = new BinImage(n,m,0);
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                (*tmp)(i,j) = f[i][j]|l;
-        return *tmp;
+        BinImage tmp(n, m, 0);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                tmp(i, j) = f[i][j] | l;
+        return tmp;
     }
-    BinImage& operator!()
+    BinImage operator!()
     {
-        BinImage* tmp = new BinImage(n,m,0);
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                (*tmp)(i,j) = !(f[i][j]);
-        return *tmp;
+        BinImage tmp(n, m, 0);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                tmp(i, j) = !(f[i][j]);
+        return tmp;
     }
-    double coefficient()
+    double coefficient() const
     {
         int true_amount = 0;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                if(f[i][j])
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (f[i][j])
                     true_amount += 1;
-        return (double)true_amount/(n*m);
+        return (double)true_amount / (n * m);
     }
     //copy constructor
-    BinImage(const BinImage &b);
-    BinImage &operator=(const BinImage &b);
+    BinImage(const BinImage &b) : n(b._n()), m(b._m())
+    {
+        f = (int **)malloc(n * sizeof(int));
+        for (int i = 0; i < n; i++)
+            f[i] = (int *)malloc((m + 1) * sizeof(int));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                f[i][j] = b(i, j);
+    }
+    BinImage &operator=(const BinImage &b)
+    {
+        if (&b == this)
+            return *this;
+        for (int i = 0; i < n; i++)
+            free(f[i]);
+        free(f);
+        f = (int **)malloc(n * sizeof(int));
+        for (int i = 0; i < n; i++)
+            f[i] = (int *)malloc((m + 1) * sizeof(int));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                f[i][j] = b(i, j);
+        return *this;
+
+    }
     //move constructor
-    BinImage(BinImage &&b);
+    BinImage(BinImage &&b): n(b._n()), m(b._m())
+    {
+        for (int i = 0; i < n; i++)
+            free(f[i]);
+        free(f);
+        f = b.f;
+    }
     BinImage &operator=(BinImage &&b);
 
-    friend std::ostream& operator<< (std::ostream &out, const BinImage &image)
+    friend std::ostream &operator<<(std::ostream &out, const BinImage &image)
     {
-        // head
-        std::cout << lt;
-        for (int j = 0; j < image.m; j++)
-            std::cout << hl;
-        std::cout << rt << std::endl;
-        // body
-        for (int i = 0; i < image.n; i++)
-        {
-            std::cout << vl;
-            for (int j = 0; j < image.m; j++)
-                if (image.f[i][j] == 1)
-                    std::cout << tv;
-                else
-                    std::cout << fv;
-            std::cout << vl << std::endl;
-        }
-        // bottom
-        std::cout << lb;
-        for (int j = 0; j < image.m; j++)
-            std::cout << hl;
-        std::cout << rb << std::endl;
+        image.output();
     }
 };
-BinImage& operator*(int l, BinImage& b)
+BinImage operator*(const int l, const BinImage &b)
 {
-    BinImage *tmp = new BinImage(b._n(),b._m(),0);
-    for(int i=0;i<b._n();i++)
-        for(int j=0;j<b._m();j++)
-            (*tmp)(i,j) = b._f()[i][j]&l;
-    return *tmp;
+    BinImage tmp(b._n(), b._m(), 0);
+    for (int i = 0; i < b._n(); i++)
+        for (int j = 0; j < b._m(); j++)
+            tmp(i, j) = b._f()[i][j] & l;
+    return tmp;
 }
-BinImage& operator+(int l, BinImage& b)
+BinImage operator+(const int l, const BinImage &b)
 {
-    BinImage *tmp = new BinImage(b._n(),b._m(),0);
-    for(int i=0;i<b._n();i++)
-        for(int j=0;j<b._m();j++)
-            (*tmp)(i,j) = b._f()[i][j]|l;
-    return *tmp;
+    BinImage tmp(b._n(), b._m(), 0);
+    for (int i = 0; i < b._n(); i++)
+        for (int j = 0; j < b._m(); j++)
+            tmp(i, j) = b._f()[i][j] | l;
+    return tmp;
 }
 int main()
 {
-    BinImage a;
-    a.input();
-    a.output();
-    std::cout << a;
+    BinImage a(4, 4, 1);
+    BinImage b(4, 4, 0);
     return 0;
 }
